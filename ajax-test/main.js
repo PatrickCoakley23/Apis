ad269cb6115a3400bada89dda1f1a452514826c0
PatrickCoakley23 <patrickcoakley23@gmail.com>
2020-05-27 08:41:57 +0000

    created table rows and truncated the data to make it fit

--- ajax-test/main.js
+++ ajax-test/main.js
@@ -1,21 +1,49 @@
-const baseUrl = "https://swapi.dev/api/";
+const baseURL = "https://swapi.dev/api/";
 
 function getData(type, cb) {
- var xhr = new XMLHttpRequest();
-
-    xhr.open("GET", baseURL + type + "/");
-    xhr.send();
+    var xhr = new XMLHttpRequest();
 
     xhr.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             cb(JSON.parse(this.responseText));
-
         }
     };
+
+    xhr.open("GET", baseURL + type + "/");
+    xhr.send();
+}
+
+function getTableHeaders(obj) {
+    var tableHeaders = [];
+
+    Object.keys(obj).forEach(function(key) {
+        tableHeaders.push(`<td>${key}</td>`)
+    });
+
+    return `<tr>${tableHeaders}</tr>`;
 }
 
 function writeToDocument(type) {
-    getData(type, function(data) {
-        document.getElementById("data").innerHtml = data;
+    var tableRows = [];
+    var el = document.getElementById("data");
+    el.innerHTML = "";
+
+    getData(type, function (data) {
+        data = data.results;
+        var tableHeaders = getTableHeaders(data[0]);
+
+        data.forEach(function (item) {
+            var dataRow = [];
+
+            Object.keys(item).forEach(function (key) {
+                var rowData = item[key].toString();
+                var truncatedData = rowData.substring(0, 15)
+                dataRow.push(`<td>${truncatedData}</td>`);
+
+          });
+          tableRows.push(`<tr>${dataRow}</tr>`);
+        });
+
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
     });
 }
